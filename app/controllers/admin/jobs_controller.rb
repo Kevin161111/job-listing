d359d5be64773e5e6b930a8e02231f4b5c79987c
Kevin161111 <hwha2008@hotmail.com>
2017-01-07 14:08:14 +0800

    implement add admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,9 +1,10 @@
-class JobsController < ApplicationController
-
+class Admin::JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_filter :require_is_admin
   def index
     @jobs = Job.all
   end
+
   def new
     @job = Job.new
   end
@@ -11,7 +12,6 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
-
   def edit
     @job = Job.find(params[:id])
   end
@@ -19,34 +19,31 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
     if @job.save
-
-    redirect_to jobs_path
+      redirect_to admin_jobs_path
     else
-    render :new
-  end
+      render :new
+    end
   end
 
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
-      redirect_to jobs_path, notice: "Update Success"
+      redirect_to admin_jobs_path
     else
       render :edit
     end
-    end
+  end
 
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
-      flash[:alert] = "Job Deleted"
-      redirect_to jobs_path
-    end
-
+    redirect_to admin_jobs_path
+  end
 
 
   private
-
   def job_params
     params.require(:job).permit(:title, :description)
   end
+
 end
